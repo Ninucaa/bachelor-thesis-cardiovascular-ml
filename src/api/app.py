9e1ea4f89b5ef342177b0f9abf4b4bdba17e940b
Ninucaa <n_jincharadze4@cu.edu.ge>
2026-06-17 01:53:43 +0400

    Polish clinical interface styling

--- src/api/app.py
+++ src/api/app.py
@@ -28,6 +28,7 @@ app.add_middleware(
         "http://127.0.0.1:5173",
         "http://localhost:5173",
     ],
+    allow_origin_regex=r"http://(127\.0\.0\.1|localhost):517\d",
     allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
